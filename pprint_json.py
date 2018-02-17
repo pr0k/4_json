790257616cf89da60248e5f80255e9b30a272c90
pr01k <pr01k@yandex.ru>
2018-02-17 20:12:57 +0300

    updated script

--- pprint_json.py
+++ pprint_json.py
@@ -1,13 +1,12 @@
 import json
-
+from sys import argv
 
 def load_data(filepath):
-    pass
-
+    with open(filepath, 'r') as f:
+        return json.load(f)
 
 def pretty_print_json(data):
-    pass
-
+    return json.dumps(data, ensure_ascii=False, indent=4, sort_keys=True)
 
 if __name__ == '__main__':
-    pass
+        print(pretty_print_json(load_data(argv[1])))
