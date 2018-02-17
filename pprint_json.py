e09a78f23644bf6070cfa96a49d438dd949eb1b2
pr01k <pr01k@yandex.ru>
2018-02-17 22:08:50 +0300

    renamed variables

--- pprint_json.py
+++ pprint_json.py
@@ -2,11 +2,11 @@ import json
 from sys import argv
 
 def load_data(filepath):
-    with open(filepath, 'r') as f:
-        return json.load(f)
+    with open(filepath, 'r') as json_file:
+        return json.load(json_file)
 
-def pretty_print_json(data):
-    return json.dumps(data, ensure_ascii=False, indent=4, sort_keys=True)
+def pretty_print_json(python_file):
+    return json.dumps(python_file, ensure_ascii=False, indent=4, sort_keys=True)
 
 if __name__ == '__main__':
         print(pretty_print_json(load_data(argv[1])))
