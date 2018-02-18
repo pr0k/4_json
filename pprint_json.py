79afa1ac3154c93c4d8f2cedf0123072ba8e35cb
pr01k <pr01k@yandex.ru>
2018-02-18 11:49:05 +0300

    added comments & exception handling and also renamed variables

--- pprint_json.py
+++ pprint_json.py
@@ -1,12 +1,21 @@
 import json
 from sys import argv
 
-def load_data(filepath):
-    with open(filepath, 'r') as json_file:
-        return json.load(json_file)
+# Decodes not formatted json data into python data
+def decode_json_data(filepath):
+    with open(filepath, 'r') as json_data_file:
+        return json.load(json_data_file)
 
-def pretty_print_json(python_file):
-    return json.dumps(python_file, ensure_ascii=False, indent=4, sort_keys=True)
+# Encodes python data into a pretty JSON format
+def encode_python_data(python_data_file):
+    return json.dumps(python_data_file, ensure_ascii=False, indent=4, sort_keys=True)
 
 if __name__ == '__main__':
-        print(pretty_print_json(load_data(argv[1])))
+    try:
+        print(encode_python_data(decode_json_data(argv[1])))
+    except IOError:
+        print('file not found!\ntry again')
+    except ValueError:
+        print('there is no JSON data in the file!\nspecify the JSON data file')
+    except IndexError:
+        print('no path to file to be processed!\ntry $ python pprint_json.py <path to file>')
