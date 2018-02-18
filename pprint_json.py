76cf8a6e11f5307776c9e2d93912dd011df4f399
pr01k <pr01k@yandex.ru>
2018-02-18 18:23:00 +0300

    deleted unnecessary comments & rerenamed variables

--- pprint_json.py
+++ pprint_json.py
@@ -1,18 +1,16 @@
 import json
 from sys import argv
 
-# Decodes not formatted json data into python data
-def decode_json_data(filepath):
+def load_data(filepath):
     with open(filepath, 'r') as json_data_file:
         return json.load(json_data_file)
 
-# Encodes python data into a pretty JSON format
-def encode_python_data(python_data_file):
+def prettify_json(python_data_file):
     return json.dumps(python_data_file, ensure_ascii=False, indent=4, sort_keys=True)
 
 if __name__ == '__main__':
     try:
-        print(encode_python_data(decode_json_data(argv[1])))
+        print(prettify_json(load_data(argv[1])))
     except IOError:
         print('file not found!\ntry again')
     except ValueError:
